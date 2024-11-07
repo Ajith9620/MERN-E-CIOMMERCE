f24e5a35331429c2141b0b22804caaadf1ff1ae4
Ajith M <962014ajith@gmail.com>
2024-11-07 18:15:49 +0530

    addingvalues

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,8 +6,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api/": "https://mern-e-ciommerce-backend.vercel.app/",
-      "/uploads/": "https://mern-e-ciommerce-backend.vercel.app/",
+      "/api/": "http://localhost:5000/",
+      "/uploads/": "http://localhost:5000/",
     },
   },
 });
